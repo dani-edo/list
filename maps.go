21f0b91547631fa90d272d42e57907e21d0b41b6
dani_edo <dani.edogawa.17@gmail.com>
2025-02-20 09:50:58 +0700

    map crud

--- maps.go
+++ maps.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	websites := map[string]string{
+	websites := map[string]string{ // map key string to string value
 		"Google":   "www.google.com",
 		"Youtube":  "www.youtube.com",
 		"Facebook": "www.facebook.com",
 	}
+	fmt.Println(websites)
+	fmt.Println(websites["Facebook"]) // get value by key
+
+	websites["Twitter"] = "www.twitter.com" // assign new value
+	fmt.Println(websites)
+
+	delete(websites, "Youtube") // delete value
 	fmt.Println(websites)
 }
