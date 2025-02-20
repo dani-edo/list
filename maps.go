522a955dc643e137f49424f6c769ff713822a6ff
dani_edo <dani.edogawa.17@gmail.com>
2025-02-20 09:43:59 +0700

    map

---
+++ maps.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":   "www.google.com",
+		"Youtube":  "www.youtube.com",
+		"Facebook": "www.facebook.com",
+	}
+	fmt.Println(websites)
+}
