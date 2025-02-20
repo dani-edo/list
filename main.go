07cac41c4f35c853ad7967c7465861dbf2ac787d
dani_edo <dani.edogawa.17@gmail.com>
2025-02-20 22:17:47 +0700

    alias for map

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64 // type alias for map
+
+func (m floatMap) output() { // method of type floatMap
+	fmt.Println(m)
+}
+
 func main() {
 	// make in slices
 	userNames := make([]string, 2, 5) // make an array of length 2 and capacity 5
@@ -14,9 +20,11 @@ func main() {
 	fmt.Println(userNames)
 	fmt.Println(cap(userNames))
 
-	courseRatings := make(map[string]float64, 3) // make a map of length 3 (use make if you already know the length you will use)
+	courseRatings := make(floatMap, 3) // make a map of length 3 (use make if you already know the length you will use)
 	courseRatings["Go"] = 4.5
 	courseRatings["React"] = 5
 	courseRatings["Angular"] = 4
-	fmt.Println(courseRatings)
+	// fmt.Println(courseRatings)
+
+	courseRatings.output()
 }
