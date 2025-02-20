9677e0df46f31fbafd96a1dffbb632ef6110aa45
dani_edo <dani.edogawa.17@gmail.com>
2025-02-20 22:06:04 +0700

    make a map

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	userNames := make([]string, 2) // make and array of length 2
+	// make in slices
+	userNames := make([]string, 2, 5) // make an array of length 2 and capacity 5
 	userNames = append(userNames, "hello")
 
 	userNames[0] = "satu"
@@ -11,4 +12,11 @@ func main() {
 	// userNames[2] = "tiga"
 
 	fmt.Println(userNames)
+	fmt.Println(cap(userNames))
+
+	courseRatings := make(map[string]float64, 3) // make a map of length 3 (use make if you already know the length you will use)
+	courseRatings["Go"] = 4.5
+	courseRatings["React"] = 5
+	courseRatings["Angular"] = 4
+	fmt.Println(courseRatings)
 }
